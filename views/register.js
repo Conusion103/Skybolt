1daa5c591f3e56f7444cf0a66dd2252667706522
Nikol Velasquez <velasqueznikol10@gmail.com>
2025-08-23 13:26:20 -0500

    style modification

--- views/register.js
+++ views/register.js
@@ -75,7 +75,7 @@ const departamentos = [
 ];
 
 // Función para renderizar el formulario de registro
-export let renderRegister = (ul, main, footer) => {
+export let renderRegister = (ul, main) => {
   // Cambiar fondo y clases del body y main
   let $body = document.getElementById("body");
   $body.style.backgroundImage = "";
@@ -92,93 +92,134 @@ export let renderRegister = (ul, main, footer) => {
   // Menú de navegación
   ul.innerHTML = `
      <!-- HEADER  -->
-    <header class="fixed top-0 left-0 w-full z-50 bg-white shadow-md">
-      <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div class="flex justify-between items-center h-16">
-          <h1 class="text-3xl font-bold text-gray-800">
-            <a href="/skybolt/home#top" class="hover:text-sky-600 transition-colors duration-200">SkyBolt</a>
-          </h1>
+<header class="fixed top-0 left-0 w-full z-50 bg-white shadow-md">
+  <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+    <div class="flex justify-between items-center h-16">
+      <h1 class="text-3xl font-bold text-gray-800">
+        <a href="/skybolt/home#top" class="hover:text-sky-600 transition-colors duration-200">SkyBolt</a>
+      </h1>
 
-          <nav class="hidden md:flex space-x-6">
-            <a href="/skybolt/home#about-us" class="nav-link">About us</a>
-            <a href="/skybolt/home#testimonials" class="nav-link">Testimonials</a>
-            <a href="/skybolt/home#faq" class="nav-link">FAQ</a>
-            <a href="/skybolt/home#map" class="nav-link"">Find Us</a>
-            <a href="#contact" class="nav-link">Contact Us</a>
-            <a href="/skybolt/login" data-link class="btn-primary">Log in</a>
-          </nav>
+      <nav class="hidden md:flex space-x-6">
+        <a href="/skybolt/home#about-us" class="nav-link">About us</a>
+        <a href="/skybolt/home#testimonials" class="nav-link">Testimonials</a>
+        <a href="/skybolt/home#faq" class="nav-link">FAQ</a>
+        <a href="/skybolt/home#map" class="nav-link"">Find Us</a>
+        <a href="#contact" class="nav-link">Contact Us</a>
+        <a href="/skybolt/login" data-link class="btn-primary">Log in</a>
 
-          <button id="menu-btn" class="md:hidden flex flex-col space-y-1">
-            <span class="w-6 h-0.5 bg-gray-800"></span>
-            <span class="w-6 h-0.5 bg-gray-800"></span>
-            <span class="w-6 h-0.5 bg-gray-800"></span>
-          </button>
-        </div>
-      </div>
+      </nav>
 
-      <!-- MENÚ MÓVIL -->
-        <div id="mobile-menu" class="hidden md:hidden w-full bg-white px-6 pb-6 flex flex-col items-center space-y-4 text-center">
-            <a href="#about-us" class="nav-link">About us</a>
-            <a href="#testimonials" class="nav-link">Testimonials</a>
-            <a href="#faq" class="nav-link">FAQ</a>
-            <a href="#map" class="nav-link">Find Us</a>
-            <a href="#contact" class="nav-link">Contact Us</a>
-            <a href="/skybolt/register" data-link class="btn-primary">Sign up</a>
-        </div>
-    </header>
-    <div id="top" class="h-16"></div>
+      <button id="menu-btn" class="md:hidden flex flex-col space-y-1">
+        <span class="w-6 h-0.5 bg-gray-800"></span>
+        <span class="w-6 h-0.5 bg-gray-800"></span>
+        <span class="w-6 h-0.5 bg-gray-800"></span>
+      </button>
+    </div>
+  </div>
+
+  <!-- MENÚ MÓVIL -->
+    <div id="mobile-menu" class="hidden md:hidden w-full bg-white px-6 pb-6 flex flex-col items-center space-y-4 text-center">
+        <a href="#about-us" class="nav-link">About us</a>
+        <a href="#testimonials" class="nav-link">Testimonials</a>
+        <a href="#faq" class="nav-link">FAQ</a>
+        <a href="#map" class="nav-link">Find Us</a>
+        <a href="#contact" class="nav-link">Contact Us</a>
+        <a href="/skybolt/register" data-link class="btn-primary">Sign up</a>
+    </div>
+
+</header>
+
+<!-- ESPACIO PARA QUE EL HEADER NO TAPE EL CONTENIDO -->
+<div id="top" class="h-16"></div>
   `;
 
   // Toggle menú móvil
-  document.getElementById("menu-btn").addEventListener("click", () => {
-    const menu = document.getElementById("mobile-menu");
-    menu.classList.toggle("hidden");
-  });
+    document.getElementById("menu-btn").addEventListener("click", () => {
+        const menu = document.getElementById("mobile-menu");
+        menu.classList.toggle("hidden");
+    });
 
   // Formulario de registro
   main.innerHTML = `
-    <section class="flex flex-col items-center justify-center min-h-screen">
-      <div class="bg-white rounded-2xl shadow-2xl w-[90%] max-w-md p-8 space-y-6">
-        <header class="text-center">
-          <h1 class="text-4xl font-bold text-green-400">REGISTER</h1>
-        </header>
-        <form id="form-register" class="space-y-4">
-          <input type="text" id="name-register" placeholder="Full Name" required class="input" />
-          <input type="email" id="email-register" placeholder="Email" required class="input" />
-          <input type="number" id="phone-register" placeholder="Phone number" required class="input" />
-          <input type="date" id="birthday-register" required class="input" />
-          <select id="documents-select" required class="input">
-            <option value="">--Type of document--</option>
-            <option value="CC">Cédula de ciudadanía</option>
-            <option value="CE">Cédula de extranjería</option>
-            <option value="PEP">Permiso especial de permanencia</option>
-          </select>
-          <input type="number" id="document-register" placeholder="Document" required class="input"/>
-          <select id="departament-register" required class="input">
-            <option value="">--Select a department--</option>
-          </select>
-          <select id="town-register" required class="input">
-            <option value="">--Select a municipality--</option>
-          </select>
-          <input type="password" id="password-register" placeholder="Password" required class="input"/>
-          <input type="password" id="password-register_" placeholder="Confirm Password" required class="input"/>
-          <button type="submit" id="button-register" class="btn-primary w-full">REGISTER</button>
-          <p class="text-center text-sm text-gray-600">Already a member? 
-            <a href="/skybolt/login" class="text-blue-500 font-semibold">Log in</a>
-          </p>
-        </form>
-      </div>
-    </section>
+  <section class="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+    <div class="bg-white rounded-2xl shadow-2xl w-[90%] max-w-md p-8 space-y-6">
+      
+      <!-- Header -->
+      <header class="text-center">
+        <h1 class="text-4xl font-bold text-green-400">REGISTER</h1>
+      </header>
+
+      <!-- Formulario -->
+      <form id="form-register" class="space-y-4">
+
+        <input type="text" id="name-register" placeholder="Full Name" required
+          class="w-full px-4 py-3 rounded-md bg-gray-200 focus:outline-none focus:ring-2 focus:ring-green-300"/>
+
+        <input type="email" id="email-register" placeholder="Email" required
+          class="w-full px-4 py-3 rounded-md bg-gray-200 focus:outline-none focus:ring-2 focus:ring-green-300"/>
+
+        <input type="number" id="phone-register" placeholder="Phone number" required
+          class="w-full px-4 py-3 rounded-md bg-gray-200 focus:outline-none focus:ring-2 focus:ring-green-300"/>
+
+        <input type="date" id="birthday-register" required
+          class="w-full px-4 py-3 rounded-md bg-gray-200 focus:outline-none focus:ring-2 focus:ring-green-300"/>
+
+        <select id="documents-select" required
+          class="w-full px-4 py-3 rounded-md bg-gray-200 focus:outline-none focus:ring-2 focus:ring-green-300">
+          <option value="">--Type of document--</option>
+          <option value="CC">Cédula de ciudadanía</option>
+          <option value="CE">Cédula de extranjería</option>
+          <option value="PEP">Permiso especial de permanencia</option>
+        </select>
+
+        <input type="number" id="document-register" placeholder="Document" required
+          class="w-full px-4 py-3 rounded-md bg-gray-200 focus:outline-none focus:ring-2 focus:ring-green-300"/>
+
+        <select id="departament-register" required
+          class="w-full px-4 py-3 rounded-md bg-gray-200 focus:outline-none focus:ring-2 focus:ring-green-300">
+          <option value="">--Select a department--</option>
+        </select>
+
+        <select id="town-register" required
+          class="w-full px-4 py-3 rounded-md bg-gray-200 focus:outline-none focus:ring-2 focus:ring-green-300">
+          <option value="">--Select a municipality--</option>
+        </select>
+
+        <input type="password" id="password-register" placeholder="Password" required
+          class="w-full px-4 py-3 rounded-md bg-gray-200 focus:outline-none focus:ring-2 focus:ring-green-300"/>
+
+        <input type="password" id="password-register_" placeholder="Confirm Password" required
+          class="w-full px-4 py-3 rounded-md bg-gray-200 focus:outline-none focus:ring-2 focus:ring-green-300"/>
+
+        <!-- Botón -->
+        <button type="submit" id="button-register"
+          class="w-full py-3 bg-green-400 text-white font-bold rounded-full hover:bg-green-500 transition flex items-center justify-center gap-2">
+          REGISTER
+        </button>
+
+        <!-- Login redirect -->
+        <p class="text-center text-sm text-gray-600">
+          Already a member? <a href="/skybolt/login" class="text-blue-500 font-semibold">Log in</a>
+        </p>
+      </form>
+    </div>
+  </section>
   `;
 
-  // Footer
-  footer.innerHTML = `
+    footer.innerHTML = `
+    <!-- FOOTER COMPLETO -->
     <footer id="contact" class="bg-[#111827] text-green-100 py-10 px-6 sm:px-10 w-full mt-30">
         <div class="max-w-7xl mx-auto grid grid-cols-1 md:grid-cols-3 gap-8">
+            
+            <!-- DESCRIPCIÓN -->
             <div>
                 <h3 class="text-xl font-bold text-white mb-4">SKYBOLT</h3>
-                <p class="text-sm">Your trusted platform to book sports venues in seconds. Technology that connects active communities.</p>
+                <p class="text-sm">
+                    Your trusted platform to book sports venues in seconds. Technology that connects active communities.
+                </p>
             </div>
+
+            <!-- ENLACES -->
             <div>
                 <h4 class="text-lg font-semibold text-white mb-3">Useful Links</h4>
                 <ul class="space-y-2 text-sm">
@@ -187,6 +228,8 @@ export let renderRegister = (ul, main, footer) => {
                     <li><a href="/skybolt/home#map" class="hover:text-yellow-300 transition">Location</a></li>
                 </ul>
             </div>
+
+            <!-- REDES -->
             <div>
                 <h4 class="text-lg font-semibold text-white mb-3">Follow Us</h4>
                 <div class="flex gap-4">
@@ -195,34 +238,43 @@ export let renderRegister = (ul, main, footer) => {
                     <a href="#" class="hover:text-yellow-300 transition">Twitter</a>
                 </div>
             </div>
+
         </div>
-        <div class="text-center text-sm mt-10 text-green-300">© 2025 SKYBOLT. All rights reserved</div>
+
+        <!-- COPYRIGHT -->
+        <div class="text-center text-sm mt-10 text-green-300">
+            © 2025 SKYBOLT. All rights reserved
+        </div>
     </footer>
-  `;
+`;
 
   // Referencias a selects
   const departamentoSelect = document.getElementById("departament-register");
   const municipioSelect = document.getElementById("town-register");
 
-  // Llenar select de departamentos
+  // Llenar select de departamentos con IDs como value
   departamentos.forEach((dep) => {
     const option = document.createElement("option");
-    option.value = dep.id;
-    option.textContent = dep.name;
+    option.value = dep.id; // valor es el ID
+    option.textContent = dep.name; // texto visible es el nombre
     departamentoSelect.appendChild(option);
   });
 
-  // Evento para actualizar municipios
+  // Evento para actualizar municipios cuando se selecciona un departamento
   departamentoSelect.addEventListener("change", (e) => {
     const selectedDepId = Number(e.target.value);
-    municipioSelect.innerHTML = '<option value="">--Select a municipality--</option>';
-    if (!selectedDepId) return;
+    municipioSelect.innerHTML =
+      '<option value="">--Selecciona un municipio--</option>';
+
+    if (!selectedDepId) return; // No seleccionado
+
     const departamento = departamentos.find((dep) => dep.id === selectedDepId);
     if (!departamento) return;
+
     departamento.municipios.forEach((muni) => {
       const option = document.createElement("option");
-      option.value = muni.id;
-      option.textContent = muni.name;
+      option.value = muni.id; // valor es el id del municipio
+      option.textContent = muni.name; // texto visible es el nombre
       municipioSelect.appendChild(option);
     });
   });
@@ -230,19 +282,28 @@ export let renderRegister = (ul, main, footer) => {
   // Evento para el botón de registro
   document.getElementById("button-register").addEventListener("click", (e) => {
     e.preventDefault();
+
     try {
       let $name = document.getElementById("name-register").value.trim();
       let $email = document.getElementById("email-register").value.trim();
       let $phone = document.getElementById("phone-register").value.trim();
-      let $birthdate = document.getElementById("birthday-register").value.trim();
+      let $birthdate = document
+        .getElementById("birthday-register")
+        .value.trim();
       let $type = document.getElementById("documents-select").value;
-      let $identification = document.getElementById("document-register").value.trim();
-      let $departament = document.getElementById("departament-register").value.trim();
+      let $identification = document
+        .getElementById("document-register")
+        .value.trim();
+      let $departament = document
+        .getElementById("departament-register")
+        .value.trim();
       let $town = document.getElementById("town-register").value.trim();
       let $password = document.getElementById("password-register").value.trim();
-      let $confirmPassword = document.getElementById("password-register_").value.trim();
+      let $confirmPassword = document
+        .getElementById("password-register_")
+        .value.trim();
 
-      // Validaciones
+      // Validaciones (ajusta para aceptar números como string si es necesario)
       generalFormat.nameFormat($name);
       generalFormat.hotmailFormat($email);
       generalFormat.phoneNumber($phone);
@@ -253,7 +314,7 @@ export let renderRegister = (ul, main, footer) => {
       generalFormat.townFormat($town);
       generalFormat.passwordFormat($password, $confirmPassword);
 
-      // Usuario
+      // Armar el usuario con IDs numéricos para departamento y municipio
       let user = {
         full_name: $name,
         email: $email,
@@ -267,18 +328,22 @@ export let renderRegister = (ul, main, footer) => {
         rol: "user",
       };
 
-      // Validar y enviar
+      // Validar si usuario ya existe consultando usuarios
       Api.get("/api/users")
         .then((users) => {
           let user_exist = users.some(
             (d) =>
               d.email === user.email ||
-              (d.document_type === user.document_type && d.id_document === user.id_document)
+              (d.document_type === user.document_type &&
+                d.id_document === user.id_document)
           );
+
           if (user_exist) throw new Error("User already exists");
+
           return Api.post("/api/users", user);
         })
-        .then(() => {
+        .then((res) => {
+          // En tu método Api.request ya devuelves json, no res.ok
           alert("Usuario registrado correctamente");
         })
         .catch((error) => {
@@ -290,4 +355,3 @@ export let renderRegister = (ul, main, footer) => {
     }
   });
 };
-
