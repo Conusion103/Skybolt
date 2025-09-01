a3c1ffd0ef2214f80eb0b0573075d6b01a3f8895
Nikol Velasquez <159703935+Nikotastic@users.noreply.github.com>
2025-09-01 17:04:42 -0500

    Update methodsApi.js
    changing of the base url

--- src/scripts/methodsApi.js
+++ src/scripts/methodsApi.js
@@ -1,6 +1,6 @@
 export let Api = {
-    // base: 'https://skybolt-production.up.railway.app',
-    base: 'http://localhost:3000',
+     base: 'https://skybolt-production.up.railway.app',
+    // base: 'http://localhost:3000',
 
     // Función para manejar la lógica repetida
     request: (method, param, pack = null) => {
@@ -40,3 +40,4 @@ export let Api = {
         return Api.request('DELETE', param);  // Llama a request con el método DELETE
     }
 };
+
